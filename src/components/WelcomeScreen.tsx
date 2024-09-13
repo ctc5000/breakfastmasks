f20021203c345af7c89535ae3266c53159ce9f32
Dosybi <dosybi@gmail.com>
2024-09-13 13:22:10 +0500

    fix button width on welcome screen

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -9,7 +9,7 @@ const WelcomeScreen: FC<WelcomeScreenProps> = ({ onClick }) => {
     <div className="w-svw h-svh relative">
       <img className="w-full h-full" src="/images/welcome.png" />
       <button
-        className="absolute left-1/2 -translate-x-1/2 text-white font-bold bg-[#FF1994] rounded-full bottom-8 px-16 py-3"
+        className="absolute left-1/2 -translate-x-1/2 text-white font-bold bg-[#FF1994] rounded-full bottom-8 px-16 py-3 w-max"
         onClick={onClick}
       >
         Включить магию!
